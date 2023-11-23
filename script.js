985e80f3eea544aa6ee2ff7fc3e781f0d9b306ef
Dumpala Abhiteja <125918097+Abhiteja1@users.noreply.github.com>
2023-11-23 20:18:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function fibonacci(num) {
 // your code here
 	let fib =[0,1];
-	for(let i=2;i<50;i++){
+	for(let i=2;i<num;i++){
 		let x = fib[i-2];
 		let y = fib[i-1];
 		fib.push(x+y);
 	}
-	return (fib[num]);
+	return (fib[num-1]);
 }
 module.exports = fibonacci;
